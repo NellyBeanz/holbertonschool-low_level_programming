6216cdb49406a717228c9538fc3c0939831d1431
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-13 23:22:09 +0000

    added comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * 
+ * return: 0
+ */
+
 int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");  
+  puts("\"Programming is like building a multilingual puzzle");  
   return(0);
 }
 
