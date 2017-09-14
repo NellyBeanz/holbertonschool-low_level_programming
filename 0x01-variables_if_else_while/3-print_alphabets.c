b24441d2bb66e9677d3985d6bc789701c2243b68
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-14 22:21:17 +0000

    new script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+		putchar(c++);
+
+	c = 'A';
+
+	while (c <= 'Z')
+		putchar(c++);
+
+	putchar ('\n');
+	return (0);
+}
