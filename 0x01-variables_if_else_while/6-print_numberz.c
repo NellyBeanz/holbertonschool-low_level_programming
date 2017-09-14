beecf71cdad20f30eef172f3771a473a850bf0d2
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-14 22:38:35 +0000

    new script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+		putchar (c++);
+	putchar ('\n');
+	return (0);
+}
