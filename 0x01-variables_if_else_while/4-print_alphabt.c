7645f83b5091134b816581c364ee82ee447d98c4
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-14 22:12:56 +0000

    new script

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		putchar(c++);
+		else
+			c++;
+	}	
+		
+	putchar ('\n');
+	return (0);
+}
