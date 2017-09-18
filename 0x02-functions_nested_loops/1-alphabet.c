3e2cd03eb100bda77c346842a199e168ab9c7a4e
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-18 23:00:48 +0000

    new script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *
+ * Main - entry
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
+	return (0);
+}
