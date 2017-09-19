1a12db26e81537007f6cc66271e1ffbe57841fbd
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-19 20:25:12 +0000

    new script that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add -  adds two integers and returns sum 
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
