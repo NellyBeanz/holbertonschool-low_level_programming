1451dd8a20ea90c4da2ca05216a0daa214444d49
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-19 16:24:14 +0000

    modification

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
-/*                                                                                                                                     
- *                                                                                                                                     
- * Main - main entry                                                                                                                   
- *                                                                                                                                     
- * Return: 0                                                                                                                           
+/**
+ * Main - prints Holberton followed by a new line
+ *
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
-        _putchar('H');
-        _putchar('o');
-        _putchar('l');
-        _putchar('b');
-        _putchar('e');
-        _putchar('r');
-        _putchar('t');
-        _putchar('o');
-        _putchar('n');
-        _putchar('\n');
-
-        return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
