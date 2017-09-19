46dc1f76e4aee5e4c3a6573fa93648a42ed207e0
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-19 16:33:12 +0000

    modification

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
 #include "holberton.h"
 
 /**
+ * main - entr pointy
  *
- * Main - entry
- * 
  * Return: 0
  */
 int main(void)
 {
 	int i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
-	
+
 	_putchar('\n');
 	return (0);
 }
