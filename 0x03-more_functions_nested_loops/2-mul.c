819c0a38ee3c760ff4610b8908a135037c3a9907
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-21 20:16:43 +0000

    new script that multiplies two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: value of two number multiplied
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
