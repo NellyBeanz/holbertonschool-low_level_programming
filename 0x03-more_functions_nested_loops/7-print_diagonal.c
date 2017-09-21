9dca315cfdf61f8de219124868fd2663ef853bb7
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-21 21:20:46 +0000

    new script for diagonal line

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws diagonal line
+ * @n: number of times \ should be printed
+ */
+void print_diagonal(int n)
+{
+	int k;
+	int m;
+
+	if (n > 0)
+		for (k = 0; k < n; k++)
+	{
+		for (m = 0; m < k; m++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
