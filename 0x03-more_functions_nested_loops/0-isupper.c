72eec0dd369ada5944ae8b8c97dd7279d2792c04
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-21 19:55:36 +0000

    new script for check for uppercase character

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: character used for uppercase
+ *
+ * Return: 1 if uppercase or 0 if not
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
