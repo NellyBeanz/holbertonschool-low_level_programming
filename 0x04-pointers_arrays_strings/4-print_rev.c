c5e7160c9f4619356d90dc3d4c024a67edd245dd
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-09-26 20:58:50 +0000

    new script that prints string in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (a = a - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
