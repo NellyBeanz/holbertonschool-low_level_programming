cb56fc66832aa5ad4be3c41cf5f77a2d004ed855
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-02 01:57:44 +0000

    modified

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -8,13 +8,15 @@
  */
 char *rot13(char *s)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char en[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
+	i = 0;
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (alpha[j] != '\0')
 		{
 			if (s[i] == alpha[j])
