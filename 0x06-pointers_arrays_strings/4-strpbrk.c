a5e7fc1edc29ad728a746a25af4ba6508f923cc6
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-03 01:35:39 +0000

    script that searches a string for any of a set of bytes

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: characters we are looking for
+ *
+ * Return: pointer to the first occurrence or NULL if no such byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+	int b;
+	char *p;
+
+	p = s;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for(b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+				return (p + a);
+		}
+	}
+	return (NULL);
+}
