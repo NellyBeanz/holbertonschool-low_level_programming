ab1b4d410e533ff76423d71a6086ff02e476d9e2
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-05 02:20:23 +0000

    script that returns length of a string

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string used to find length
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
