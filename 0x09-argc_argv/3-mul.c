29c407d5fbccf7066d6b22c716bd8f91ddeab2b5
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-09 05:45:14 +0000

    script that multiples two numbers

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiples two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	int multiply;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		multiply = atoi(argv[1]) * atoi[argv[2]);
+		printf("%d\n", multiply);
+		return (0);
+	}
+}
