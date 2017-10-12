02c22aa8e4c740dd3c17a9076d43c89038bfd014
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-12 22:19:06 +0000

    added NULL for s1 and s2

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -31,6 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int i, j, count;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	if (n > _strlen(s2))
 		n = _strlen(s2);
 
