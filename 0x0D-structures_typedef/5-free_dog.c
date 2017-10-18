ac0395ce054bbf344903de200ed5ac60bd2947a2
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-18 00:13:32 +0000

    function that frees members from allocated memory

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dog(allocated memory)
+ * @d: pointer to dog_t which is a type of struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d != NULL)
+	{
+		free(d);
+		free(d->name);
+		free(d->owner);
+	}
+}
