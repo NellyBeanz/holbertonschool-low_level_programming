a00d1da27f920b6041404bd381a916c055e5297c
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-20 21:28:24 +0000

    new script that prints numbers

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, unsigned int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
