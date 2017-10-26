2ef58ade491416b8902a0c661e291fc54348f7bd
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-26 23:31:06 +0000

    pushed header file and moved incrementation to original place

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -15,11 +15,11 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
+		i++;
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		i++;
 		h = h->next;
 	}
 	return (i);
--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef LINKED_LISTS_H
 #define LINKED_LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
