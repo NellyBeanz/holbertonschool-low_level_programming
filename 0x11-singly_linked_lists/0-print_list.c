b3573c26f9019e04fa4e45abd1e1ee8fe23158db
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-26 23:26:52 +0000

    moved incrementation

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -12,13 +12,14 @@ size_t print_list(const list_t *h)
 	i = 0;
 	if (h == NULL)
 		return (0);
+
 	while (h != NULL)
 	{
-		i++;
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
+		i++;
 		h = h->next;
 	}
 	return (i);
