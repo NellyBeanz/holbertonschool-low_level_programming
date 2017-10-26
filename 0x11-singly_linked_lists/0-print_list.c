1c2241272dd2a187ae56bcddf62d412734d087ec
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-26 23:17:38 +0000

    added NULL statement and changed to unsigned int

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -7,16 +7,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	unsigned int i;
 
 	i = 0;
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
 		i++;
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (i);
