fbc5dfa00615ae47d6686b8b55067a8712e05939
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-26 23:07:13 +0000

    increase count outside of loop and removed == NULL condition

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -3,22 +3,20 @@
 /**
  * print_list - prints all the elements of list
  * @h: pointer
- * Return: length
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t i;
 
 	i = 0;
-	if (h == NULL)
-		return (0);
-
 	while (h != NULL)
 	{
+		i++;
 		if (h->str == NULL)
-			i += printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			i += printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (i);
