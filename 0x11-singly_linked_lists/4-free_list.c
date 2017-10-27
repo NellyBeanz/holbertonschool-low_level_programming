56889b139c7def4bc1579cb61e5304d56fab24b0
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-27 17:53:54 +0000

    new script that frees list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list
+ * @head: pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head);
+}
