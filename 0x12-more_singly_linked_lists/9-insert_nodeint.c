dfc24c9cd25f4557096e106267f6e54090cf9f9c
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-31 22:30:08 +0000

    changed NULL statement for *head to tmp and deleted tmp statement

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,13 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	i = 1;
 	tmp = *head;
-	if (tmp == NULL)
-	{
-		free(new);
-		return(NULL);
-	}
-
-	while (*head != NULL)
+	while (tmp != NULL)
 	{
 		if (i != idx)
 		{
