fd3553b39da41d0576915460679418f0b96ecc31
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-31 01:31:00 +0000

    script that returns nth node of a list

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that gets nth node of list
+ * @head: pointer
+ * @index: takes in an index
+ *
+ * Return: nth node of list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	if (head == NULL)
+		return (NULL);
+
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
