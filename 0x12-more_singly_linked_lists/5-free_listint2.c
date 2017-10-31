e80a9c60808100893d7e9bae49d2fcbce370f645
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-31 18:58:57 +0000

    changed free statement

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,10 @@ void free_listint2(listint_t **head)
 
 	while ((*head) != NULL)
 	{
-		tmp = *head;
-		*head = tmp->next;
-		free(tmp);
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
-	free(*head);
+	free(tmp);
 	*head = NULL;
 }
