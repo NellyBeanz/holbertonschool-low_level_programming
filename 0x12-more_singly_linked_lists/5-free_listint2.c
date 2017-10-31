b53548ba9cbe6efb5dd6173173d5352e2ec1fe11
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-10-31 19:11:06 +0000

    changed free

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,9 +13,9 @@ void free_listint2(listint_t **head)
 
 	while ((*head) != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
 	}
-	free(tmp);
+	free(*head);
 }
