d064f11930a742bebf749dd7da1fe0b1ddd1ec75
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-11-08 00:23:17 +0000

    changed if statement

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (length > 0)
+	if (text_content != 0)
 		writ = write(fd, text_content, length);
 
 	if (writ == -1)
