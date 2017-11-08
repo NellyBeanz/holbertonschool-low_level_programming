18f47532cc4dbeba2d751553ac8e670fe81e1a5b
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-11-08 00:08:20 +0000

    script that creates a file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 for success or -1 if fails
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, length, writ;
+
+	if (filename == NULL)
+		return (-1);
+
+	length = 0;
+	while (text_content[length] != '\0')
+	{
+		length++;
+	}
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (length > 0)
+		writ = write(fd, text_content, length);
+
+	if (writ == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
