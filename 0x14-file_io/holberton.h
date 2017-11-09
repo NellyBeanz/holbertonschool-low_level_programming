7fe9d5ba99283860a8fc847004769a02c92e6946
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-11-09 00:01:33 +0000

    added buffer

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#define BUFFSIZE 1024
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -7,6 +8,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
