f4c23a130ebad6385475662a74b6acf3c5b503c5
Nelly Ochoa <ochoa_nelly@outlook.com>
2017-11-09 00:32:51 +0000

    rearranged brackets

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -15,16 +15,16 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from from_to\n"), exit(97);
 	if (argv[1] == NULL)
-	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98); }
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(from), exit(98); }
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+			exit(98);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	buf = malloc(sizeof(char *) * BUFFSIZE);
+	buf = malloc(sizeof(char) * BUFFSIZE);
 	if (buf == NULL)
 		return (-1);
 	do {
